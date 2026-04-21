465bd824eb304127874117a6495828bcaa3c1093
successinc <success.inc22@gmail.com>
2026-04-21 14:50:55 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export", // গিটহাব পেজেসের জন্য এটি জরুরি
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // বিল্ডের সময় এরর এড়াতে এটি যোগ করা ভালো
+  },
+  images: {
+    unoptimized: true, // গিটহাব পেজেসে ছবি দেখানোর জন্য এটি প্রয়োজন
   },
   reactStrictMode: false,
 };
